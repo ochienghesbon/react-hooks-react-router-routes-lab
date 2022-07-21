3b33c77a8bc4cb1f629fd4061b8b453293649982
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-07-22 02:16:33 +0300

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,20 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movies Page</h1>
+    {movies.map((movie, index) =>{
+      return <div key={index}>
+        <h3>Name: {movie.title}</h3>
+        <p>Time: {movie.time}</p>
+        Genres: <ul>
+          {movie.genres.map((genre, index)=>{
+            return <li key={index}>{genre}</li>
+          })}
+        </ul>
+      </div>
+    })}
+  </div>;
 }
 
 export default Movies;
